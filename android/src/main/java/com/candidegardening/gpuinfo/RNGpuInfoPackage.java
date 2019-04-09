b10eb1505f92d4f7591e233fb07a6bf7f815d078
David Branton <david.branton@candide.eu>
2019-04-09 13:07:41 +0100

    Export the whole es6 module
    Don't faux type

--- android/src/main/java/com/candidegardening/gpuinfo/RNGpuInfoPackage.java
+++ android/src/main/java/com/candidegardening/gpuinfo/RNGpuInfoPackage.java
@@ -5,10 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNGpuInfoPackage implements ReactPackage {
     @Override
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-
 import { NativeModules } from 'react-native'
 
 const { RNGpuInfo } = NativeModules
 
-/**
- * Get the name of the GL renderer.
- */
-export function getGlRenderer() {
-    return RNGpuInfo.getGlRenderer()
-}
+export default RNGpuInfo
