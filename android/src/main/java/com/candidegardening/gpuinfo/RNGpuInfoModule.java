876c795fdf6de76e8ee232673c1d287f3047ab1b
David Branton <david.branton@candide.eu>
2019-04-09 11:55:27 +0100

    Remove unused import
    Make the gradlew file executable

--- android/src/main/java/com/candidegardening/gpuinfo/RNGpuInfoModule.java
+++ android/src/main/java/com/candidegardening/gpuinfo/RNGpuInfoModule.java
@@ -6,7 +6,6 @@ import android.preference.PreferenceManager;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNGpuInfoModule extends ReactContextBaseJavaModule {
   private final ReactApplicationContext reactContext;
